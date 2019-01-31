347e87365164ffb6726299799ad8c6b3ed0face9
Noah Paige <noahlandonpaige@gmail.com>
2019-01-31 11:33:30 -0800

    Added shadow mapping for players

---
+++ Lightspeed/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    private new Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") != 0)
+            animator.SetBool("isRunning", true);
+        else
+            animator.SetBool("isRunning", false);
+
+        if (Input.GetAxis("Vertical") != 0)
+            animator.SetBool("isJumping", true);
+        else
+            animator.SetBool("isJumping", false);
+
+    }
+}
---
+++ Lightspeed/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D playerBody;
+
+    //add bounds for these 2
+    public float speed;
+    public float jumpPower;
+
+    private bool canJump = false;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (spriteRenderer != null && Mathf.Abs(playerBody.velocity.x) > 0.001f)
+        {
+            spriteRenderer.flipX = playerBody.velocity.x < 0.0f;
+        }
+
+        playerBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerBody.velocity.y);
+        if (Input.GetKey(KeyCode.Space) && canJump)
+        {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, jumpPower);
+            canJump = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("PlayerMovementController.OnCollisionEnter2D -- collided with " + collision.gameObject.tag);
+
+        if (collision.gameObject.CompareTag("collidable"))
+        {
+            Collider2D collider = collision.collider;
+
+            Vector3 contactPoint = collision.contacts[0].point;
+            Vector3 center = collider.bounds.center;
+
+            //bool right = contactPoint.x > center.x;
+            //bool top = contactPoint.y > center.y;
+            if(contactPoint.y > center.y)
+            {
+                canJump = true;
+            }
+            
+        }
+    }
+}
