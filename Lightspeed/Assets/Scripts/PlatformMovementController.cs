809a4af56e9e15e25b511cc50350cdeddf2be74e
Noah Paige <noahlandonpaige@gmail.com>
2019-02-18 13:01:20 -0800

    Changed the way platformcontroller works.

--- Lightspeed/Assets/Scripts/PlatformMovementController.cs
+++ Lightspeed/Assets/Scripts/PlatformMovementController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PlatformMovementController : MonoBehaviour
 {
-
     public Vector2[] points; // list of points to lerp along
     public float startMovingPoint;
     public float endMovingPoint;
     public GameObject player;
     public float ease;
+    public float tileWidth = 0.19531f;
     private Rigidbody2D rb;
 
     private int fromPoint;
     private int toPoint;
-    private float prevPercentage; // the % representing where the player is between startMovingPoint and endMovingPoint
-    private float actualPercentage; // the % representing where the platform is between points[0] and points[points.length - 1]
+    private float prevPercentage = 0f; // the % representing where the player is between startMovingPoint and endMovingPoint
+
+    private bool playerWasInWindow = false;
+    private Vector2 prevPlayerPos = Vector2.zero;
 
     // Use this for initialization
     void Start()
@@ -31,32 +33,64 @@ public class PlatformMovementController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rb.MovePosition(Ease(CalcTranslation(player.transform.position)));
+        //rb.MovePosition(Ease(CalcTranslation(player.transform.position)));
+        rb.MovePosition(Ease(CalcX(player.transform.position)));
     }
 
-    Vector2 CalcTranslation(Vector2 playerPos)
+    Vector2 CalcX(Vector2 playerPos)
     {
-        float length = Mathf.Abs(startMovingPoint - endMovingPoint);
+        //check if player is in window
+        Vector2 output = transform.position;
+        if (playerPos.x >= startMovingPoint && playerPos.x <= endMovingPoint)
+        {
+            if (playerWasInWindow)
+            {
+                //percentage = calc percentage player has moved inside window based on current player and previous player positions
+                float playerMoveDist = playerPos.x - prevPlayerPos.x;
+                float percentage = playerMoveDist / Mathf.Abs(startMovingPoint - endMovingPoint);
+                percentage += prevPercentage;
+                output = TryMovePlatform(percentage);
+            }
+            prevPlayerPos = playerPos;
+            playerWasInWindow = true;
+        }
+        else playerWasInWindow = false;
 
-        float percentage;
-        if (startMovingPoint < endMovingPoint) percentage = (player.transform.position.x - startMovingPoint) / length; // OVERALL percentage
-        else percentage = (startMovingPoint - player.transform.position.x) / length; // OVERALL percentage
-        float adjustedPercentage = (percentage - (fromPoint / (points.Length - 1))) * (points.Length - 1) - fromPoint; // percentage between fromPoint and toPoint
-        actualPercentage = (float)Mathf.Min(fromPoint, toPoint) / (points.Length - 1) +
-                            (Vector3.Distance(points[fromPoint], transform.position) / Vector3.Distance(points[fromPoint], points[toPoint])) / (points.Length - 1);
 
-        Debug.Log("PlatformMovementController - CalcTranslation : Percentage = " + percentage + " adjusted = " + adjustedPercentage + " actual = " + actualPercentage);
-        //(float)(points.Length - 1 - Mathf.Max(fromPoint, toPoint)) / (points.Length - 1);
+        return output;
+    }
+    //takes overall percentage
+    Vector2 TryMovePlatform(float percentage)
+    {
         UpdateFromAndTo(percentage);
+        float adjustedPercentage = (percentage - (fromPoint / (points.Length - 1))) * (points.Length - 1) - fromPoint; // percentage between fromPoint and toPoint
+        //Debug.Log("percentage: " + percentage);
+        Vector2 moveTowards;
+        moveTowards = Vector2.Lerp(points[fromPoint], points[toPoint], adjustedPercentage);
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(moveTowards, new Vector2(2f * tileWidth, tileWidth), transform.rotation.z);
 
-        //Debug.Log("ObjectMovementController.updateFromAndTo -- ADJUSTED PERCENTAGE = " + percentage);
-        return Vector2.Lerp(points[fromPoint],
-                            points[toPoint],
-                            adjustedPercentage);
+
+        float savedPrevPercentage = prevPercentage;
+        foreach(Collider2D col in colliders)
+        {
+
+            
+            if (col.CompareTag("collidable") && col.gameObject.transform.parent.gameObject != this.gameObject)
+            {
+                Debug.Log("found " + col.gameObject.name);
+                UpdateFromAndTo(prevPercentage);
+                //moveTowards = new Vector2(rb.position.x + );
+                prevPercentage = savedPrevPercentage;
+                break;
+            }
+            else prevPercentage = percentage;
+        }
+        return moveTowards;
     }
 
+   
     // takes the OVERALL percentage
     void UpdateFromAndTo(float percentage)
     {
@@ -95,23 +129,17 @@ public class PlatformMovementController : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         float gizWidth = 0.1f;
-        //Gizmos.color = Color.red;
+        Gizmos.color = Color.red;
 
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Gizmos.DrawLine(new Vector3(points[i].x, points[i].y, -1f), new Vector3(points[i + 1].x, points[i + 1].y, -1f));
+        }
         for (int i = 0; i < points.Length; i++)
         {
-            Gizmos.color = Color.red;
             Gizmos.DrawSphere(new Vector3(points[i].x, points[i].y, -1f), gizWidth);
         }
+        
 
     }
-
-    private void MoveChildrenRigidbodies(Vector2 pos)
-    {
-        foreach (Transform child in transform)
-        {
-            Rigidbody2D rb = child.GetComponent<Rigidbody2D>();
-            rb.MovePosition(pos); // + new Vector2(child.transform.position.x, child.transform.position.y));
-        }
-    }
-
 }
