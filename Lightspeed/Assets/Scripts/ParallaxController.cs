d989d53af51623f536f5aa15f4f569ecfd87acad
Noah Paige <noahlandonpaige@gmail.com>
2019-03-15 09:39:52 -0700

    Finished Level2

---
+++ Lightspeed/Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour {
+
+    public GameObject player;
+
+    [Range(1,128)]
+    public int moveFactor = 8;
+
+    private Vector3 prevPosition;
+
+	// Use this for initialization
+	void Start () {
+        prevPosition = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 toPos = transform.position + (player.transform.position - prevPosition);
+        transform.position = Vector3.Lerp(transform.position, toPos, 1f / (float)moveFactor);
+
+        prevPosition = player.transform.position;
+	}
+}
